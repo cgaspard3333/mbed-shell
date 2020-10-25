bf7d91252d1317f921e88989aec1920a8f3585c8
Gregwar <g.passault@gmail.com>
2020-10-25 13:42:16 +0100

    Shell

--- src/shell.cpp
+++ src/shell.cpp
@@ -1,3 +1,474 @@
+#include <mbed.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "shell.h"
 
-// ...
+static mbed::Stream *stream = nullptr;
+static bool disabled = false;
+
+
+/**
+ * Global variables shell
+ */
+
+static char shell_buffer[SHELL_BUFFER_SIZE];
+
+static bool shell_last_ok = false;
+static unsigned int shell_last_pos = 0;
+static unsigned int shell_pos = 0;
+
+static const struct shell_command *shell_commands[SHELL_MAX_COMMANDS];
+
+static unsigned int shell_command_count = 0;
+
+static bool shell_echo_mode = true;
+
+/**
+ * Registers a command
+ */
+void shell_register(const struct shell_command *command)
+{
+    shell_commands[shell_command_count++] = command;
+}
+
+static void displayHelp(bool parameter)
+{
+    char buffer[256];
+    unsigned int i;
+
+    if (parameter) {
+        shell_stream()->puts("Available parameters:");
+    } else {
+        shell_stream()->puts("Available commands:");
+    }
+    shell_println();
+
+    for (i=0; i<shell_command_count; i++) {
+        const struct shell_command *command = shell_commands[i];
+
+        if (command->parameter != parameter) {
+            continue;
+        }
+
+        int namesize = strlen(command->name);
+        int descsize = strlen(command->description);
+        int typesize = (command->parameter_type == NULL) ? 0 : strlen(command->parameter_type);
+
+        memcpy(buffer, command->name, namesize);
+        buffer[namesize++] = ':';
+        buffer[namesize++] = '\r';
+        buffer[namesize++] = '\n';
+        buffer[namesize++] = '\t';
+        memcpy(buffer+namesize, command->description, descsize);
+        if (typesize) {
+            buffer[namesize+descsize++] = ' ';
+            buffer[namesize+descsize++] = '(';
+            memcpy(buffer+namesize+descsize, command->parameter_type, typesize);
+            buffer[namesize+descsize+typesize++] = ')';
+        }
+        buffer[namesize+descsize+typesize++] = '\r';
+        buffer[namesize+descsize+typesize++] = '\n';
+        shell_stream()->write(buffer, namesize+descsize+typesize);
+    }
+}
+
+/**
+ * Internal helping command
+ */
+SHELL_COMMAND(help, "Displays the help about commands")
+{
+    displayHelp(false);
+}
+
+void shell_params_show()
+{
+    unsigned int i;
+
+    for (i=0; i<shell_command_count; i++) {
+        const struct shell_command *command = shell_commands[i];
+
+        if (command->parameter) {
+            command->command(0, NULL);
+        }
+    }
+}
+
+/**
+ * Display available parameters
+ */
+SHELL_COMMAND(params, "Displays the available parameters. Usage: params [show]")
+{
+    if (argc && strcmp(argv[0], "show")==0) {
+        shell_params_show();
+    } else {
+        displayHelp(true);
+    }
+}
+
+/**
+ * Switch echo mode
+ */
+SHELL_COMMAND(echo, "Switch echo mode. Usage echo [on|off]")
+{
+    if ((argc == 1 && strcmp("on", argv[0])) || shell_echo_mode == false) {
+        shell_echo_mode = true;
+        shell_stream()->puts("Echo enabled");
+    } else {
+        shell_echo_mode = false;
+        shell_stream()->puts("Echo disabled");
+    }
+}
+
+/**
+ * Write the shell prompt
+ */
+void shell_prompt()
+{
+    stream->puts(SHELL_PROMPT);
+}
+
+const struct shell_command *shell_find_command(char *command_name, unsigned int command_name_length)
+{
+    unsigned int i;
+
+    for (i=0; i<shell_command_count; i++) {
+        const struct shell_command *command = shell_commands[i];
+
+        if (strlen(command->name) == command_name_length && strncmp(shell_buffer, command->name, command_name_length) == 0) {
+            return command;
+        }
+    }
+
+    return NULL;
+}
+
+/***
+ * Executes the given command with given parameters
+ */
+bool shell_execute(char *command_name, unsigned int command_name_length, 
+        unsigned int argc, char **argv)
+{
+    unsigned int i;
+    const struct shell_command *command;
+
+    // Try to find and execute the command
+    command = shell_find_command(command_name, command_name_length);
+    if (command != NULL) {
+        command->command(argc, argv);
+    }
+
+    // If it fails, try to parse the command as an allocation (a=b)
+    if (command == NULL) {
+        for (i=0; i<command_name_length; i++) {
+            if (command_name[i] == '=') {
+                command_name[i] = '\0';
+                command_name_length = strlen(command_name);
+                command = shell_find_command(command_name, command_name_length);
+
+                if (command && command->parameter) {
+                    argv[0] = command_name+i+1;
+                    argv[1] = NULL;
+                    argc = 1;
+                    command->command(argc, argv);
+                } else {
+                    command = NULL;
+                }
+
+                if (!command) {
+                    stream->puts("Unknown parameter: ");
+                    stream->write(command_name, command_name_length);
+                    shell_println();
+                    return false;
+                }
+            }
+        }
+    }
+
+    // If it fails again, display the "unknown command" message
+    if (command == NULL) {
+        stream->puts("Unknown command: ");
+        stream->write(command_name, command_name_length);
+        shell_println();
+        return false;
+    }
+
+    return true;
+}
+
+/***
+ * Process the receive buffer to parse the command and executes it
+ */
+void shell_process()
+{
+    char *saveptr;
+    unsigned int command_name_length;
+
+    unsigned int argc = 0;
+    char* argv[SHELL_MAX_ARGUMENTS+1];
+
+    shell_println();
+
+    strtok_r(shell_buffer, " ", &saveptr);
+    while (
+            (argv[argc] = strtok_r(NULL, " ", &saveptr)) != NULL && 
+            argc < SHELL_MAX_ARGUMENTS
+          ) {
+        *(argv[argc]-1) = '\0';
+        argc++;
+    }
+
+    if (saveptr != NULL) {
+        *(saveptr - 1) = ' ';
+    }
+
+    command_name_length = strlen(shell_buffer);
+
+    if (command_name_length > 0) {
+        shell_last_ok = shell_execute(shell_buffer, command_name_length, argc, argv);
+    } else {
+        shell_last_ok = false;
+    }
+
+    shell_last_pos = shell_pos;
+    shell_pos = 0;
+    shell_prompt();
+}
+
+/**
+ * Save the Serial object globaly
+ */
+void shell_init(mbed::Stream *stream_)
+{
+    stream = stream_;
+    shell_prompt();
+}
+
+mbed::Stream *shell_stream()
+{
+    return stream;
+}
+
+void shell_reset()
+{
+    shell_pos = 0;
+    shell_last_pos = 0;
+    shell_buffer[0] = '\0';
+    shell_last_ok = false;
+    shell_prompt();
+}
+
+/**
+ * Stops the shell
+ */
+void shell_disable()
+{
+    disabled = true;
+}
+
+void shell_enable()
+{
+    shell_last_ok = false;
+    disabled = false;
+}
+
+/**
+ * Ticking the shell, this will cause lookup for characters 
+ * and eventually a call to the process function on new lines
+ */
+void shell_tick()
+{
+    if (disabled || stream == nullptr) {
+        return;
+    }
+
+    char c;
+    uint8_t input;
+
+    while (stream->readable()) {
+        input = stream->getc();
+        c = (char)input;
+        if (c == '\0') {
+            continue;
+        }
+
+        //Return key
+        if (c == '\r' || c == '\n') {
+            if (shell_pos == 0 && shell_last_ok) { 
+                // If the user pressed no keys, restore the last 
+                // command and run it again
+                unsigned int i;
+                for (i=0; i<shell_last_pos; i++) {
+                    if (shell_buffer[i] == '\0') {
+                        shell_buffer[i] = ' ';
+                    }
+                }
+                shell_pos = shell_last_pos;
+            }
+            shell_buffer[shell_pos] = '\0';
+            shell_process();
+            //Back key
+        } else if (c == '\x7f') {
+            if (shell_pos > 0) {
+                shell_pos--;
+                stream->puts("\x8 \x8");
+            }
+            //Special key
+        } else if (c == '\x1b') {
+            while (!stream->readable());
+            stream->getc();
+            while (!stream->readable());
+            stream->getc();
+            //Others
+        } else {
+            shell_buffer[shell_pos] = c;
+            if (shell_echo_mode) {
+                stream->putc(c);
+            }
+
+            if (shell_pos < SHELL_BUFFER_SIZE-1) {
+                shell_pos++;
+            }
+        }
+    }
+}
+
+
+float shell_atof(char *str)
+{
+    int sign = (str[0]=='-') ? -1 : 1;
+    float f = atoi(str);
+    char *savePtr;
+    strtok_r(str, ".", &savePtr);
+    char *floatPart = strtok_r(NULL, ".", &savePtr);
+    if (floatPart != NULL) {
+        float divide = 1;
+        for (char *tmp=floatPart; *tmp!='\0'; tmp++) {
+            divide *= 10; 
+        }   
+        f += sign*atoi(floatPart)/divide;
+    }
+
+    return f;
+}
+
+void shell_println()
+{
+    shell_stream()->puts("\r\n");
+}
+
+
+void shell_print_bool(bool value)
+{
+    if (value) {
+        shell_stream()->puts("true");
+    } else {
+        shell_stream()->puts("false");
+    }
+}
+
+void shell_print(char *s)
+{
+    stream->puts(s);
+}
+
+void shell_print(char c)
+{
+    stream->putc(c);
+}
+
+void shell_print(unsigned long long n, uint8_t base)
+{
+    unsigned char buf[CHAR_BIT * sizeof(long long)];
+    unsigned long i = 0;
+
+    if (n == 0) {
+        shell_stream()->putc('0');
+        return;
+    }
+
+    while (n > 0) {
+        buf[i++] = n % base;
+        n /= base;
+    }
+
+    for (; i > 0; i--) {
+        shell_stream()->putc((char)(buf[i - 1] < 10 ?
+                     '0' + buf[i - 1] :
+                     'A' + buf[i - 1] - 10));
+    }
+}
+
+void shell_print(long long n, uint8_t base)
+{
+    if (n < 0) {
+        shell_print('-');
+        n = -n;
+    }
+    shell_print((unsigned long long)n, base);
+}
+
+void shell_print(int n, uint8_t base)
+{
+    if (n < 0) {
+        shell_print('-');
+        n = -n;
+    }
+    shell_print((unsigned long long)n, base);
+}
+
+void shell_print(unsigned int n, uint8_t base)
+{
+    shell_print((unsigned long long)n, base);
+}
+
+#define LARGE_DOUBLE_TRESHOLD (9.1e18)
+
+void shell_print(double number, int digits)
+{
+    // Hackish fail-fast behavior for large-magnitude doubles
+    if (abs(number) >= LARGE_DOUBLE_TRESHOLD) {
+        if (number < 0.0) {
+            shell_stream()->putc('-');
+        }
+        shell_stream()->puts("<large double>");
+        return;
+    }
+
+    // Handle negative numbers
+    if (number < 0.0) {
+        shell_stream()->putc('-');
+        number = -number;
+    }
+
+    // Simplistic rounding strategy so that e.g. print(1.999, 2)
+    // prints as "2.00"
+    double rounding = 0.5;
+    for (uint8_t i = 0; i < digits; i++) {
+        rounding /= 10.0;
+    }
+    number += rounding;
+
+    // Extract the integer part of the number and print it
+    long long int_part = (long long)number;
+    double remainder = number - int_part;
+    shell_print(int_part);
+
+    // Print the decimal point, but only if there are digits beyond
+    if (digits > 0) {
+        shell_stream()->putc('.');
+    }
+
+    // Extract digits from the remainder one at a time
+    while (digits-- > 0) {
+        remainder *= 10.0;
+        int to_print = (int)remainder;
+        shell_print(to_print);
+        remainder -= to_print;
+    }
+}
+
+void shell_print(bool b)
+{
+    shell_print_bool(b);
+}
---
+++ src/shell.h
@@ -0,0 +1,162 @@
+#ifndef _RHOBAN_SHELL_H
+#define _RHOBAN_SHELL_H
+
+#include <Stream.h>
+#include <mbed.h>
+#include <stdlib.h>
+
+/**
+ * Maximum length of a command line
+ * and its argument
+ */
+#define SHELL_BUFFER_SIZE 64
+
+/**
+ * Maximum number of command arguments
+ */
+#define SHELL_MAX_ARGUMENTS 8
+
+/**
+ * Maximum number of commands
+ * which ca be registered
+ */
+#define SHELL_MAX_COMMANDS 100
+
+/**
+ * shell prompt
+ */
+#define SHELL_PROMPT "$ "
+
+/**
+ * Initializing shell
+ * This function have to be called before using
+ * shell_tick()
+ *
+ * @param com : you have to provide to shell_init
+ * an initialized instance of USBSerial or HardwareSerial
+ */
+void shell_init(mbed::Stream *stream);
+
+/**
+ * Get the shell stream instance
+ */
+mbed::Stream *shell_stream();
+
+/**
+ * Resets the shell
+ */
+void shell_reset();
+
+/**
+ * Activate or desactivate the shell
+ */
+void shell_disable();
+void shell_enable();
+
+/**
+ * shell ticking
+ * Call this function in your main loop 
+ * to fetch serial port and handle shell
+ */
+void shell_tick();
+
+/**
+ * Mute the shell
+ */
+void shell_silent(bool silent);
+
+/**
+ * ----------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------
+ */
+
+/**
+ * Prototype of a shell command
+ */
+typedef void shell_command_fn(unsigned int argc, char *argv[]);
+
+/**
+ * A command definition for the shell
+ */
+struct shell_command
+{
+    char *name;
+    char *description;
+    shell_command_fn *command;
+    bool parameter;
+    char *parameter_type;
+};
+
+/**
+ * Registers a command
+ */
+void shell_register(const struct shell_command *command);
+
+#define SHELL_COMMAND_INTERNAL(name, description, parameter, parameterType) shell_command_fn shell_command_ ## name; \
+    \
+    char shell_command_name_ ## name [] = #name; \
+    char shell_command_description_ ## name [] = description; \
+    \
+    struct shell_command shell_command_definition_ ## name = { \
+        shell_command_name_ ## name , \
+        shell_command_description_ ## name , \
+        shell_command_ ## name, \
+        parameter, \
+        parameterType \
+    }; \
+    \
+    __attribute__((constructor)) \
+    void shell_command_init_ ## name () {  \
+        shell_register(&shell_command_definition_ ## name ); \
+    } \
+    \
+    void shell_command_ ## name (unsigned int argc, char *argv[])
+
+#define SHELL_COMMAND(name, description) \
+    SHELL_COMMAND_INTERNAL(name, description, false, NULL)
+
+#define SHELL_PARAMETER(name, description, startValue, type, conversion) \
+    volatile static type name = startValue; \
+    char shell_parameter_type_ ## name [] = #type; \
+    \
+    SHELL_COMMAND_INTERNAL(name, description, true, shell_parameter_type_ ## name) \
+    { \
+        type g; \
+        if (argc) { \
+            g = conversion(argv[0]); \
+            name = g; \
+        } \
+        shell_print( #name ); \
+        shell_print("="); \
+        shell_print(name); \
+        shell_println(); \
+    }
+
+float shell_atof(char *str);
+
+#define SHELL_PARAMETER_FLOAT(name, description, startValue) \
+    SHELL_PARAMETER(name, description, startValue, float, shell_atof)
+
+#define SHELL_PARAMETER_DOUBLE(name, description, startValue) \
+    SHELL_PARAMETER(name, description, startValue, double, shell_atof)
+
+#define SHELL_PARAMETER_INT(name, description, startValue) \
+    SHELL_PARAMETER(name, description, startValue, int, atoi)
+
+#define SHELL_PARAMETER_BOOL(name, description, startValue) \
+    SHELL_PARAMETER(name, description, startValue, bool, (bool)atoi)
+
+// Utility functions
+void shell_print(char *s);
+void shell_print(char c);
+void shell_print(unsigned long long n, uint8_t base=10);
+void shell_print(long long n, uint8_t base=10);
+void shell_print(int n, uint8_t base=10);
+void shell_print(unsigned int n, uint8_t base=10);
+void shell_print(double d, int digits=2);
+void shell_print(bool b);
+void shell_println();
+
+#endif // _shell_H
+
